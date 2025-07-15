ebd3973484d73b3b1bab8082db2543b3189113cf
BabyShaliniChandran <Baby.Chandran@Coforge.com>
2025-07-15 17:13:06 +0530

    Update the code

--- shalini_decode.py
+++ shalini_decode.py
@@ -4,4 +4,4 @@ Name=data[:4].upper()
 add_nums=int(data[4:6])+8
 multiply_num=float(data[9:13])*2
 Reversed_title=data[-1:-7:-1]
-print(f"Name: {Name}\n42 + 8 = {add_nums}\n3.14 * 2 = {multiply_num}\nReversed_title: {Reversed_title}")
+print(f"Name: {Name}\n42 + 8 = {add_nums}\n3.14 * 2 = {multiply_num}\nReversed title: {Reversed_title}")
